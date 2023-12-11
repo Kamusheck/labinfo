6c51b21ec5deb4ef064703759b7bd04a741bf54e
Kamusheck <kismailova722@gmail.com>
2023-12-11 12:51:01 +0300

    add report.md

--- lab_12/task11.c
+++ lab_12/task11.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 bool f(int num){
+  if (-10 < num && num < 10){
+    return true;
+  }
     int last_digit = num % 10;
-    num = num / 10;
+    num =abs(num) / 10;
     
     while (num > 0){
         int this_digit = num % 10;
         
-        if (last_digit >= this_digit){
+        if (abs(last_digit) >= abs(this_digit)){
             return true;
         }
         else {
